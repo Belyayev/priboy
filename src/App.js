ce977f4f250407a145a0e14467be7769ca271358
Mike <4xgood@gmail.com>
2020-12-28 12:44:59 -0700

    added basic text

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Поздравляем с Новым 2021 годом!</h2>
+      <h1>ИП Прибой</h1>
+      <p>г.Уральск</p>
+      <p>priboy2005@mail.ru</p>
     </div>
   );
 }
