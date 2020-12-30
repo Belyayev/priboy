7b4139df92deb5ed040d9ff9dc7d1789a274845c
Mike <4xgood@gmail.com>
2020-12-30 10:36:29 -0700

    added melody

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import useSound from 'use-sound';
+
+import music from './sound/Happy_New_Year.mp3';
 
 function App() {
+
+  const [melody] = useSound(music);
+
   return (
     <div className="App">
       <h2>Поздравляем с Новым 2021 годом!</h2>
       <h1>ИП Прибой</h1>
       <p>г.Уральск</p>
       <p>priboy2005@mail.ru</p>
+      <button onClick={melody}>melody</button>
     </div>
   );
 }
